edfc61a1318149fbc19b988517312fcc4779bae6
Mayuri <123patilmayuri>
2016-08-02 14:00:34 +0530

    nginx metadata

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,5 @@ description      'Installs/Configures nginx-install'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-dependes 'nginx'
+depends 'nginx'
+
